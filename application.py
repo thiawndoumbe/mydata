86f1fb2090ed9ad1d38ba65a8ebe3ceeff1f9956
ndoumbe <thiawndoumbe@gmail.comgit config --global user.email thiawndoumbe@gmail.com>
2022-06-05 19:39:52 +0000

    deuxieme fonction def f(a,b):     s = a+b     print(s) f(4,5)
    r1=f(4,5)
    r2 = g(4,5)
    print(r1)
    print(r2)

---
+++ application.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 25 14:51:55 2022
+
+@author: ndoumbe
+"""
+
+def g(a,b):
+    s= a+b
+    return s
+g(4,5)
+
+
+def f(a,b):
+    s = a+b
+    print(s)
+f(4,5) 
+
+r1=f(4,5)   
+r2 = g(4,5)
+print(r1)
+print(r2)
