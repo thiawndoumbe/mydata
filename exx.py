4cbf2510c779fa27a80e40615c455e4d437e9cfb
ndoumbe <thiawndoumbe@gmail.comgit config --global user.email thiawndoumbe@gmail.com>
2022-06-05 19:03:31 +0000

    deuxieme sauvegarde

---
+++ exx.py
@@ -0,0 +1,274 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 23 10:00:19 2022
+
+@author: ndoumbe
+"""
+"""
+# Exercice9
+coordinates=[(4,3),(9,3),(12,7),(13,7),(18,6),(20,19)]
+ordinates=[]
+dict_coordinate={}
+d2={}
+for i in coordinates :
+    c=(i[0])
+    v=i[1]
+    dict_coordinate[v]=c
+print(d2) 
+print(type(d2)) 
+"""
+  
+
+
+
+"""
+i=0
+dict_coordinates={}
+while i < len(coordonnes)
+coordinates=[(4,3),(9,3),(12,7),(13,7),(18,6),(20,7)]   
+i=0    
+while i< len(coordinates) :
+    tpl1=coordinates[i]
+    #print(tpl1[1])#
+    print(coordinates[i][1])
+    i=i+1
+"""  
+"""  
+coordinates=[(4,3),(9,3),(12,7),(13,7),(18,6),(20,19)]
+i=0
+dict_coordina
+while i< len(coordinates):
+    tpl1=coordinates[i]
+    c=tpl1[0]
+    v=tpl1[1]
+    dict_coordinates[c]=v
+    i=i+1
+print(dict_coordinates)   
+"""
+"""
+coordinates=[(4,5),(9,3),(12,8),(13,7),(18,6),(20,9)]
+i=0
+coordinates2=[]
+while i< len(coordinates):
+     point=coordinates[i]
+     y=point[1]
+     x=point[0]
+     if y>7:
+         nouveau_point=(x,7)
+     else:
+         nouveau_point=(x,y)
+     coordinates2.append(nouveau_point)    
+     i=i+1
+print(coordinates2)     
+"""
+"""
+coordinates=[(4,3),(9,3),(12,7),(13,7),(18,6),(20,19)]
+coordinates2=[]
+for i in coordinates :
+    y=i[1]
+    x=i[0]
+    if y>7:
+         nouveau_point=(x,7)
+    else:
+         nouveau_point=(x,y)
+    coordinates2.append(nouveau_point)    
+print(coordinates2) 
+"""
+# Exercice9
+"""
+chaine="Ce beau beau beau pays Un pays que je veux remettre sur pied"
+res=chaine.replace("je veux","Assim veut")
+print(res)
+res=chaine.split()
+print(res)
+dict_mots={}
+for i in res:
+    if i not in dict_mots.keys():
+        dict_mots[i]=1
+    else:
+        dict_mots[i] +=1
+print(dict_mots) 
+"""
+"""
+def f(ch):
+    res=ch.replace("je veux","Assim veut")
+    return res
+print(f("je veux manger"))
+
+chaine="Ce beau beau beau pays Un pays que"
+def h(chaine):
+    res=chaine.split()
+    return res
+print(h("je veux ce beau"))
+
+ch="Ce beau beau beau pays Un pays que"
+res=ch.split()
+dict_mots={}
+def g(ch):
+    for i in res:
+        if i not in dict_mots.keys():
+            dict_mots[i]=1
+        else:
+            dict_mots[i] +=1
+d1=f("je veux ce pays")   
+d2=h("je veux ce beau ")         
+ """           
+   
+#exercice10
+c=0
+n+1
+from math import *
+for i in range(0,n):
+    x=math()
+    y=math()
+    if x * x+y*y<=1:
+        c=c+1
+print(4*c/n)
+    
+
+ 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
